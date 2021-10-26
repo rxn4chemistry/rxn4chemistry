3cbeab37fbb06b157fe67a8d4260ccaaad93cfb4
Matteo Manica <drugilsberg@gmail.com>
2021-10-26 11:15:11 +0200

    chore: bumping package version.
    Signed-off-by: Matteo Manica <drugilsberg@gmail.com>

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -21,7 +21,7 @@ copyright = "2021, RXN for Chemistry team"
 author = "RXN for Chemistry team"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 
